642669660a352ef7f880c7be75c429d033c69b95
kamal <kamalkerbaj2020.gmail.com>
2022-11-01 18:46:45 +0200

    this is my first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function (){})
+$(document).ready(function(){})
+document.getElementById("idname")
+document.getElementsByClassName('classname')
+document.getElementsByTagName('p')
+$(',classname')
+$('p')
+$('#idname')
+var button = document.getElementById('idname')
+button.addEventListener('click',function(){})
+$('#idname').click(functio(){})
+$('p').css({"backgroud-color" : "white","display": "none"})
+document.getElementsByTagName('p').style.display='non';
+document.getElementsByTagName('p').style.background='white';
+$(',classname').hide();
+$('img').attr("src","img2.png");
+
+$('img').attr("width", "100");
+$(',classname').height(50);
